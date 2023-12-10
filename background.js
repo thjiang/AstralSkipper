ac160fdcb392a1ea4068adee214057558026176e
thjiang <browserify@qq.com>
2023-12-10 17:46:37 +0800

    :sparkles: block zhihu/juejin/jianshu/csdn

---
+++ background.js
@@ -0,0 +1,23 @@
+chrome.webNavigation.onBeforeNavigate.addListener(details => {
+  console.log(details);
+  const blockedDomains = [
+    { domain: "www.jianshu.com", param: "url" },
+    { domain: "links.jianshu.com", param: "to" },
+    { domain: "link.zhihu.com", param: "target" },
+    { domain: "link.juejin.cn", param: "target" },
+    { domain: "link.csdn.net", param: "target" }
+  ];
+
+  const targetUrl = new URL(details.url);
+
+  for (const { domain, param } of blockedDomains) {
+    if (targetUrl.hostname === domain && targetUrl.searchParams.has(param)) {
+      const redirectUrl = decodeURIComponent(targetUrl.searchParams.get(param));
+
+      if (redirectUrl) {
+        chrome.tabs.update(details.tabId, { url: redirectUrl });
+        break; // 如果匹配到一个被阻止的域名，就不再继续检查其他域名
+      }
+    }
+  }
+});
